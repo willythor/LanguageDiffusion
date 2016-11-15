db15319a36c0a996c5b78de62ded534671983305
Sung Park <sungwoo.park@students.olin.edu>
2016-11-15 10:46:42 -0500

    Make an agent only move if a cell that it will move to is empty

--- code/model.py
+++ code/model.py
@@ -1,6 +1,6 @@
 from mesa import Agent, Model
 from mesa.time import RandomActivation
-from mesa.space import MultiGrid
+from mesa.space import SingleGrid
 import random
 import numpy as np
 import matplotlib.pyplot as plt
@@ -16,7 +16,13 @@ class LanguageAgent(Agent):
     def move(self):
         neighbors = self.model.grid.get_neighborhood(self.pos,moore=True,include_center=False)
         new_position = random.choice(neighbors)
-        self.model.grid.move_agent(self, new_position)
+        if self.model.grid.is_cell_empty():
+            self.model.grid.move_agent(self, new_position)
+
+class LanguageObject(Agent):
+    """An object that an agent identifies"""
+    def __init__(self):
+        self.name = "banana"
 
 class LanguageModel(Model):
     """A model simulating the language diffusion"""
@@ -27,7 +33,7 @@ class LanguageModel(Model):
         """
         self.num_agents = N
         #Grid third parameters - Boolean for wrapping around
-        self.grid = MultiGrid(width, height, True)
+        self.grid = SingleGrid(width, height, True)
         self.schedule = RandomActivation(self)
 
         #Create agents
@@ -35,8 +41,7 @@ class LanguageModel(Model):
         for i in range(self.num_agents):
             a = LanguageAgent(i,self)
             self.schedule.add(a)
-            x = random.randrange(self.grid.width)
-            y = random.randrange(self.grid.height)
+            x, y = self.grid.find_empty()
             self.grid.place_agent(a, (x,y))
         
     def step(self):
@@ -53,8 +58,10 @@ if __name__ == '__main__':
         agent_counts = np.zeros((example_model.grid.width, example_model.grid.height))
         for cell in example_model.grid.coord_iter():
             cell_content, x, y = cell
-            agent_count = len(cell_content)
-            agent_counts[x][y] = agent_count
+            if not cell_content:
+                agent_counts[x][y] = 1
+            #agent_count = len(cell_content)
+            #agent_counts[x][y] = agent_count
         plt.imshow(agent_counts, interpolation='nearest')
         plt.colorbar()
         plt.show()
