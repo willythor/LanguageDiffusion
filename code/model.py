e7e206225705b41c5e5c8adb2e8bed1911afdbb0
willem.thorbecke@students.olin.edu <willem.thorbecke@students.olin.edu>
2016-11-17 16:21:08 -0500

    added language diffusion capabilities

--- code/model.py
+++ code/model.py
@@ -1,6 +1,6 @@
 from mesa import Agent, Model
 from mesa.time import RandomActivation
-from mesa.space import SingleGrid
+from mesa.space import MultiGrid
 import random
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,15 +9,80 @@ class LanguageAgent(Agent):
     """An agent speaking a certain language"""
     def __init__(self,unique_id, model):
         super().__init__(unique_id,model)
+        self.objects = ["banana", "apple", "pear", "orange"]
+        self.consanants = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Z']
+        self.vowels = ['A', 'E', 'I', 'O', 'U', 'Y']
+        self.language = {}
+        self.interacting = False
     
     def step(self):
         self.move()
+        if random.choice([True, False]):
+            discovery = random.choice(self.objects)
+            if discovery in self.language:
+                self.interact(discovery)
+            else:
+                self.language[discovery] = {self.word_gen(): 1}
+                self.interact(discovery)
 
+    def interact(self, discovery):
+
+        cellmates = self.model.grid.get_cell_list_contents([self.pos])
+
+        if len(cellmates) > 1:
+            self.interacting = True
+
+            peer = random.choice(cellmates)
+            peer.interacting = True
+
+            self_word = max(self.language[discovery], key = self.language[discovery].get)
+
+            if discovery in peer.language:
+
+                peer_word = max(peer.language[discovery], key = peer.language[discovery].get)
+
+                if self_word in peer.language[discovery]:
+                    peer.language[discovery][self_word] += 1
+
+                elif peer_word in self.language[discovery]:
+                    self.language[discovery][peer_word] += 1
+
+            else: 
+                peer.language[discovery] = {max(self.language[discovery], key = self.language[discovery].get): 1}
+                peer_word = self_word
+
+            # print('peer: ' + discovery + " = " + max(peer.language[discovery], key = peer.language[discovery].get))
+            # print('instigator ' + discovery + " = " + max(self.language[discovery], key = self.language[discovery].get))
+
+            #update master list in model
+            #if object not in master list, add it
+            if discovery in self.model.language:
+                #if peer or self word in master language, increment it, else add it
+                if peer_word in self.model.language[discovery]:
+                    self.model.language[discovery][peer_word] += 1
+                elif peer_word not in self.model.language[discovery]:
+                    self.model.language[discovery][peer_word] = 1
+
+                if self_word in self.model.language[discovery]:
+                     self.model.language[discovery][self_word] += 1
+                elif self_word not in self.model.language[discovery]: 1
+
+            #if word not in master list, add self_word twice because it's now shared by the two agents in this interaction
+            else:
+                self.model.language[discovery] = {self_word: 2}
+
+
+    def word_gen(self):        
+        l1 = random.choice(self.consanants)
+        l2 = random.choice(self.vowels)
+        l3 = random.choice(self.consanants)
+        return l1+l2+l3
+            
     def move(self):
         neighbors = self.model.grid.get_neighborhood(self.pos,moore=True,include_center=False)
         new_position = random.choice(neighbors)
-        if self.model.grid.is_cell_empty():
-            self.model.grid.move_agent(self, new_position)
+        self.model.grid.move_agent(self, new_position)
+        self.interacting = False
 
 class LanguageObject(Agent):
     """An object that an agent identifies"""
@@ -26,27 +91,41 @@ class LanguageObject(Agent):
 
 class LanguageModel(Model):
     """A model simulating the language diffusion"""
+
     def __init__(self, N, width, height):
         """
             engN: Number of EnglishAgent to create
             frN: Number of FrenchAgent to create
         """
+
         self.num_agents = N
         #Grid third parameters - Boolean for wrapping around
-        self.grid = SingleGrid(width, height, True)
+        self.grid = MultiGrid(width, height, True)
         self.schedule = RandomActivation(self)
+        self.language = {}
+
+
 
         #Create agents
-        #Add english agents to upper left corner and french agents to lower right corner
+        #Add agents
         for i in range(self.num_agents):
             a = LanguageAgent(i,self)
             self.schedule.add(a)
-            x, y = self.grid.find_empty()
+            x = random.randrange(self.grid.width)
+            y = random.randrange(self.grid.height)
             self.grid.place_agent(a, (x,y))
+
+
+        # #how to collect data!???
+        # self.datacollector = DataCollector(
+        # model_reporters={"Gini": compute_gini})
         
     def step(self):
         """Advance the model by one step"""
+        print(self.language)
         self.schedule.step()
+
+
 
 
 if __name__ == '__main__':
--- code/model_vis.py
+++ code/model_vis.py
@@ -1,6 +1,7 @@
 from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
 from model import *
+from mesa.visualization.modules import ChartModule
 
 def agent_portrayal(agent):
     portrayal = {"Shape": "circle",
@@ -11,27 +12,19 @@ def agent_portrayal(agent):
                  "h": 0.5,
                  "w": 0.5}
    
-    if str(type(agent)) == "<class 'model.FrenchAgent'>":
-        portrayal["Color"] = "red"
-        portrayal["Layer"] = 0
-        portrayal["Shape"] = "rect"
-        portrayal["h"] = .5
-        portrayal["w"] = .5
-
-
-    elif str(type(agent)) == "<class 'model.EnglishAgent'>":
+    if agent.interacting:
         portrayal["Color"] = "blue"
-        portrayal["Layer"] = 1
-
+     
     return portrayal
 
 
 grid = CanvasGrid(agent_portrayal, 20, 20, 500, 500)
 
+
 server = ModularServer(LanguageModel,
                        [grid],
-                       "Language Diffusion",
-                       100, 100, 20, 20)
+                       "Language Model",
+                       100, 20, 20)
 
 server.port = 8889
 server.launch()
